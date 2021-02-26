4548f80f57dad6b3eaad210d295c815fe65f8b30
john <nashjc@uottawa.ca>
2021-02-26 09:16:54 -0500

    removing cnm-pascal and renewing with only unique elements

--- Pascal2021/cnm-pascal/00README.md
+++
@@ -1,5 +0,0 @@
-# cnm-pascal
-
-These are codes from the original (5.25") diskette that accompanied
-
-Nash, J.C. (1990). Compact Numerical Methods for Computers: linear algebra and function minimisation (2nd ed.). CRC Press.
--- Pascal2021/cnm-pascal/matcopy.pas
+++
@@ -1,9 +0,0 @@
-Procedure matcopy(nRow ,nCol: integer; A: rmatrix; var B:wmatrix);
-{matcopy.pas
-  -- copies matrix A, nRow by nCol, into matrix B }
-var i,j: integer;
-begin
-  for i:=1 to nRow do
-    for j:=1 to nCol do
-      B[i,j]:=A[i,j];
-end;{matcopy.pas}
